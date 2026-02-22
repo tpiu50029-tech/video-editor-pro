f801cd697288c69502320574faa0f077c2d93ba6
tpiu50029-tech <tpiu50029@gmail.com>
2026-02-22 01:40:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function autoSubtitles(){ alert("Legendas (visual)"); }
+function autoCut(){ alert("Corte automático (visual)"); }
+function enhanceQuality(){ alert("Melhorar qualidade (visual)"); }
+function addMusic(){ alert("Adicionar música (visual)"); }
+
+function applyTrim(){ alert("Corte aplicado (visual)"); }
+function setSpeed(){ alert("Velocidade alterada"); }
+function rotate(){ alert("Rotação aplicada"); }
+function flipH(){ alert("Espelhamento horizontal"); }
+function flipV(){ alert("Espelhamento vertical"); }
+
+function generateAutoSubtitles(){ alert("Gerar legendas (visual)"); }
+function addSubtitle(){ alert("Legenda adicionada"); }
+
+function shareToSocial(){ alert("Compartilhar (visual)"); }
+function previewExport(){ alert("Pré-visualizar (visual)"); }
+function exportVideo(){ alert("Exportar (visual)"); }
+
+function resetAll(){ location.reload(); }
+function newVideo(){ location.reload(); }
